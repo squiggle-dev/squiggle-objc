76267650a5f5d666b91b87966896cefcd44e14d1
Andy White <andy@soundintheory.co.uk>
2016-12-13 18:36:26 +0000

    Spec update

--- SquiggleApi/Model/SQUser.h
+++ SquiggleApi/Model/SQUser.h
@@ -66,6 +66,8 @@
 
 @property(nonatomic) SQSnippet* snippet;
 
+@property(nonatomic) NSNumber* snippetEnabled;
+
 @property(nonatomic) NSString* secretKey;
 
 @property(nonatomic) NSString* accessToken;
--- SquiggleApi/Model/SQUser.m
+++ SquiggleApi/Model/SQUser.m
@@ -17,7 +17,7 @@
  * This method is used by `JSONModel`.
  */
 + (JSONKeyMapper *)keyMapper {
-  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"_id": @"id", @"email": @"email", @"companyName": @"company_name", @"firstName": @"first_name", @"lastName": @"last_name", @"domain": @"domain", @"address1": @"address1", @"address2": @"address2", @"address3": @"address3", @"addressCity": @"address_city", @"addressPostcode": @"address_postcode", @"addressCountry": @"address_country", @"website": @"website", @"tel": @"tel", @"linkedin": @"linkedin", @"facebook": @"facebook", @"twitter": @"twitter", @"instagram": @"instagram", @"googleplus": @"googleplus", @"logo": @"logo", @"snippet": @"snippet", @"secretKey": @"secret_key", @"accessToken": @"access_token", @"createdAt": @"created_at", @"updatedAt": @"updated_at" }];
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"_id": @"id", @"email": @"email", @"companyName": @"company_name", @"firstName": @"first_name", @"lastName": @"last_name", @"domain": @"domain", @"address1": @"address1", @"address2": @"address2", @"address3": @"address3", @"addressCity": @"address_city", @"addressPostcode": @"address_postcode", @"addressCountry": @"address_country", @"website": @"website", @"tel": @"tel", @"linkedin": @"linkedin", @"facebook": @"facebook", @"twitter": @"twitter", @"instagram": @"instagram", @"googleplus": @"googleplus", @"logo": @"logo", @"snippet": @"snippet", @"snippetEnabled": @"snippet_enabled", @"secretKey": @"secret_key", @"accessToken": @"access_token", @"createdAt": @"created_at", @"updatedAt": @"updated_at" }];
 }
 
 /**
@@ -27,7 +27,7 @@
  */
 + (BOOL)propertyIsOptional:(NSString *)propertyName {
 
-  NSArray *optionalProperties = @[@"_id", @"email", @"companyName", @"firstName", @"lastName", @"domain", @"address1", @"address2", @"address3", @"addressCity", @"addressPostcode", @"addressCountry", @"website", @"tel", @"linkedin", @"facebook", @"twitter", @"instagram", @"googleplus", @"logo", @"snippet", @"secretKey", @"accessToken", @"createdAt", @"updatedAt"];
+  NSArray *optionalProperties = @[@"_id", @"email", @"companyName", @"firstName", @"lastName", @"domain", @"address1", @"address2", @"address3", @"addressCity", @"addressPostcode", @"addressCountry", @"website", @"tel", @"linkedin", @"facebook", @"twitter", @"instagram", @"googleplus", @"logo", @"snippet", @"snippetEnabled", @"secretKey", @"accessToken", @"createdAt", @"updatedAt"];
   return [optionalProperties containsObject:propertyName];
 }
 
